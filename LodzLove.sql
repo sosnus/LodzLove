aa69ebc64141641518453fa7ff811b769040e695
Grzegorian <grzegorzpiasta@gmail.com>
2019-09-28 00:15:28 +0200

    update database

--- LodzLove.sql
+++ LodzLove.sql
@@ -1,23 +1,36 @@
 CREATE TABLE `Sensors` (
-  `SensorId` int,
+  `SensorID` int,
   `SensorName` varchar(255),
   `SensorPassword` varchar(255),
-  `SensorDescription` varchar(255)
+  `SensorDescription` varchar(255),
+  `Gps_latitude` float8,
+  `Gps_longitude` float8,
+  `Gps_altitude` float8
 );
 
 CREATE TABLE `Measurements` (
-  `MeasurementId` int,
+  `MeasurementID` int,
   `MeasurementDate` datetime,
-  `SensorId` int,
-  `TypeId` int,
+  `SensorID` int,
+  `TypeID` int,
   `Value` double
 );
 
 CREATE TABLE `Types` (
-  `TypeId` int,
+  `TypeID` int,
   `Name` varchar(255)
 );
 
-ALTER TABLE `Measurements` ADD FOREIGN KEY (`SensorId`) REFERENCES `Sensors` (`SensorId`);
+CREATE TABLE `Users` (
+  `UsersID` int,
+  `SensorID` int,
+  `Name` varchar(255),
+  `Owner` varchar(255),
+  `Email` varchar(255)
+);
 
-ALTER TABLE `Measurements` ADD FOREIGN KEY (`TypeId`) REFERENCES `Types` (`TypeId`);
+ALTER TABLE `Measurements` ADD FOREIGN KEY (`SensorID`) REFERENCES `Sensors` (`SensorID`);
+
+ALTER TABLE `Types` ADD FOREIGN KEY (`TypeID`) REFERENCES `Measurements` (`TypeID`);
+
+ALTER TABLE `Sensors` ADD FOREIGN KEY (`SensorID`) REFERENCES `Users` (`SensorID`);
