56b2d25f49ddd7d71f189aac19b53133f95b0c44
Grzegorian <grzegorzpiasta@gmail.com>
2019-09-28 13:12:27 +0200

    Update sql pastebin backup.sql

--- sql pastebin backup.sql
+++ sql pastebin backup.sql
@@ -1,1 +1,26 @@
-SELECT MAX(`MeasurementDate`),SensorID,MeasurementID FROM `Measurements` GROUP by SensorID
+	
+	
+
+
+SELECT 
+    Sensors.SensorID,
+    Sensors.SensorName,
+    Sensors.SensorDescription,
+    Sensors.Gps_latitude,
+    Sensors.Gps_longitude,
+	Measurements.MeasurementID,
+    Measurements.TypeID,
+    Measurements.Value,
+    Measurements.MeasurementDate as newest
+FROM 
+	Measurements
+INNER JOIN Sensors on Sensors.SensorID = Measurements.SensorID
+where 
+	Measurements.MeasurementDate in (
+        select maxdate 
+            from(
+                    SELECT max(Measurements.MeasurementDate) as maxdate, Measurements.SensorID
+                    from Measurements 
+                    GROUP by Measurements.SensorID
+                ) aa
+        )
